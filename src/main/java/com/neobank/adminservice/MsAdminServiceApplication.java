b09eeef9b33dbc30f62548ef0fd8f13e95e4f8c1
Abbashatred <gitabbas26@gmail.com>
2026-05-30 23:11:57 +0400

    first  commit

---
+++ src/main/java/com/neobank/adminservice/MsAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.neobank.adminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAdminServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAdminServiceApplication.class, args);
+    }
+
+}
